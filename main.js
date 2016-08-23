990daab0860052e92f9a7e64ecdc6d5673a94b0d
Sarah Ward <sarahward@Sarahs-MacBook-Air.local>
2016-08-22 20:31:40 -0500

    iffe stuff

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-console.log(SolarSystem);
+ console.log(SolarSystem);
 
-console.log("Planets:", SolarSystem.getPlanets());
-console.log(SolarSystem.setPlanets("Alderaan", "Hoth"));
-console.log("Planets:", SolarSystem.getPlanets());
+// console.log("Planets:", SolarSystem.getPlanets());
+// console.log(SolarSystem.setPlanets("Alderaan", "Hoth"));
+// console.log("Planets:", SolarSystem.getPlanets());
 
-console.log("Spacecraft:", SolarSystem.getActiveSpacecraft());
-console.log(SolarSystem.setActiveSpacecraft("Millenium Falcon", "X-wing"));
-console.log("Spacecraft:", SolarSystem.getActiveSpacecraft());
+// console.log("Spacecraft:", SolarSystem.getActiveSpacecraft());
+// console.log(SolarSystem.setActiveSpacecraft("Millenium Falcon", "X-wing"));
+// console.log("Spacecraft:", SolarSystem.getActiveSpacecraft());
 
-console.log("Planets Visited: ", SolarSystem.getPlanetsLandedOn());
-console.log(SolarSystem.setPlanetsLandedOn(4));
-console.log("Planets Visited: ", SolarSystem.getPlanetsLandedOn());
+// console.log("Planets Visited: ", SolarSystem.getPlanetsLandedOn());
+// console.log(SolarSystem.setPlanetsLandedOn(4));
+// console.log("Planets Visited: ", SolarSystem.getPlanetsLandedOn());
+
+
+// Augmented 
+
+console.log(SolarSystem.getClosestStars());
